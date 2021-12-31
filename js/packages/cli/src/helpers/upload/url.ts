f7afe10f310102840ae329781538d8f05b302b85
sebi <gore.sebyx@yahoo.com>
2021-12-31 08:23:25 +0000

    remove console log

--- js/packages/cli/src/helpers/upload/url.ts
+++ js/packages/cli/src/helpers/upload/url.ts
@@ -2,7 +2,6 @@ export default function urlUpload(
   manifestBuffer: Buffer,
 ) {
   const json = JSON.parse(manifestBuffer.toString('utf8'));
-  console.log(json);
 
   return [json.metadataUrl, json.image];
 }
